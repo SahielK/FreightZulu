b730e131abe65d509dde82109813030d8c80e182
SahielK <164166003+SahielK@users.noreply.github.com>
2024-09-23 12:57:12 +0530

    Update payment.js

--- payment.js
+++ payment.js
@@ -2,7 +2,7 @@ const express = require("express");
 const paymentController = require("../controllers/Spayment");
 const router = express.Router();
 
-// router.get('/get-preference', profileController.getPreference);
+// router.get('/get-preference', profileController.getPreference); 23-9
 
 router.get("/getPaymentDetails", paymentController.GetPayInfo);
 router.get("/getPaypal", paymentController.PaypalAmount);
